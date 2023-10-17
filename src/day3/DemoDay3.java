04deac2178ec92569b90d8fd6e381104725b6fa4
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-10-17 13:55:56 -0400

    Added new coded

---
+++ src/day3/DemoDay3.java
@@ -0,0 +1,23 @@
+package day3;
+
+public class DemoDay3 {
+	
+	
+	
+				
+			public static void main ( String [] args) {
+				
+				System.out.println("Test");
+				
+				
+			}
+				public static void add (int Red, int blue,int white) {
+					System.out.println (Red+blue+white);
+					
+				}
+				public static void add (int Jessica) {
+					System.out.println ("Test");
+
+
+}
+}
