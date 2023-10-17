56fa7593038e0ccbcf2be7f111166dcb8c8b3559
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-10-17 18:17:36 -0400

    Modified method

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -3,7 +3,7 @@ package day3;
 public class DemoDay3 {
 	
 	
-	
+            
 				
 			public static void main ( String [] args) {
 				
@@ -14,14 +14,14 @@ public class DemoDay3 {
 				public static void add (int Red, int blue,int white) {
 					System.out.println (Red+blue+white);
 					
-				}
-				public static void add (int Jessica, int 5, int B) {
+			}
+				public static void add (int F , int B) {
 					System.out.println ("Test");
 
-
+			}
 					String country ="USA";
 					String state ="VA";
 					String name = "Jessica";
+				
+}
 
-}
-}
